6c12a8f078de8fb6ae782fe53464debfe056146a
v0idheaven <dahiyavarun2007@gmail.com>
2025-11-16 01:37:29 +0530

    style update

--- nav.js
+++ nav.js
@@ -4,11 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (hamburger && navMenu) {
         hamburger.addEventListener('click', () => {
-            // Toggle classes for menu and hamburger animation
             hamburger.classList.toggle('toggle');
             navMenu.classList.toggle('nav-active');
             
-            // Toggle a class on the body for the overlay and scroll lock
             document.body.classList.toggle('nav-open');
         });
     }
